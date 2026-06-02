76bf9cbe382d3fc2b47faeb102e47838f63b07b9
alnapk-215 <alnapk215@gmail.com>
2026-06-02 13:59:51 +0530

    Configured PostgreSQL database connection

--- fetch_news.py
+++ fetch_news.py
@@ -4,3 +4,11 @@ from textblob import TextBlob
 from datetime import datetime
 
 API_KEY = "9937956bd25b4d39a610bafcaa761aa4"
+
+conn = psycopg2.connect(
+    host="news-db.cirma8a8kvzz.us-east-1.rds.amazonaws.com",
+    database="newsdb",
+    user="postgres",
+    password="alnarajeevan",
+    port="5432"
+)
