9a254bcc7aa5059b264fa0bc7273eb720996e9d8
alnapk-215 <alnapk215@gmail.com>
2026-06-03 09:09:49 +0530

    Configured PostgreSQL database connection

--- test_db.py
+++ test_db.py
@@ -1,1 +1,9 @@
 import psycopg2
+
+conn = psycopg2.connect(
+    host="news-db.cirma8a8kvzz.us-east-1.rds.amazonaws.com",
+    database="newsdb",
+    user="postgres",
+    password="alnarajeevan",
+    port="5432"
+)
