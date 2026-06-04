2c9bad1537bc910dd9cc0f7bd0349d1a0f93b945
alnapk-215 <alnapk215@gmail.com>
2026-06-04 11:46:15 +0530

    Added config module

---
+++ lambda/modules/config.py
@@ -0,0 +1,11 @@
+# DATABASE CONFIGURATION
+
+DB_HOST = "news-db.cirma8a8kvzz.us-east-1.rds.amazonaws.com"
+DB_NAME = "newsdb"
+DB_USER = "postgres"
+DB_PASSWORD = "alnarajeevan"
+DB_PORT = 5432
+
+# NEWS API
+
+NEWS_API_KEY = "09c5ffa098854520935f6121e5f146d8"
